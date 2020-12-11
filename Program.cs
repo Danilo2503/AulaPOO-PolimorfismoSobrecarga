02bf004b9420a1631206e08c6455dc15e2209374
unknown <dannilorch@gmail.com>
2020-12-11 15:36:32 -0300

    Polimorfismo Sobrecarga

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AulaPOO_PolimorfismoSobrecarga.classes;
+
+namespace AulaPOO_PolimorfismoSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar("Tsuka");
+            fun.Mostrar(3);
+
+            Console.Beep();
+        }
+    }
+}
